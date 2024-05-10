cf3670492ce0f9cb415d983d4834318010274a37
VolodymyrKolisnichenko <90705859+vk-workshop@users.noreply.github.com>
2024-05-10 18:04:32 +0200

    make build

---
+++ index.4787d829.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=t.getBoundingClientRect();t.style.top=e.clientHeight/2-l.height/2+"px",t.style.left=e.clientWidth/2-l.width/2+"px";
+//# sourceMappingURL=index.4787d829.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const spiderParams = spider.getBoundingClientRect();
+spider.style.top = wall.clientHeight / 2 - spiderParams.height / 2 + "px";
+spider.style.left = wall.clientWidth / 2 - spiderParams.width / 2 + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
